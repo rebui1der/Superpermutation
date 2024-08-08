477e09dda55b4668cf6c3266a5f79cffc2dd894e
Vladislav <151129693+rebui1der@users.noreply.github.com>
2024-08-08 10:52:35 +0300

    Update superpermutation_fastest.py
    New version

--- superpermutation_fastest.py
+++ superpermutation_fastest.py
@@ -1,30 +1,46 @@
 import itertools as it
 
+def arr_range(n: int) -> list:
+    arr = []
+    div = n
+    end = 0
+    for k in range(1, size):
+        div //= size - k + 1
+        for j in range(size - k):
+            end += div
+        arr += [end]
+    arr += [n]
+    return arr
+
+def num_in_range(arr: list, number: int) -> int:
+    for i, val in enumerate(arr):
+        if number < val:
+            return i + 1
+
 size = 9
-letters = [chr(i) for i in range(65, 65 + size)]
+
+letters = tuple(chr(i) for i in range(65, 65 + size))
 print(f'letters = {letters}')
+perm_dict = {''.join(val) : i for i, val in enumerate(it.permutations(letters))}
+perm_tuple = tuple(perm_dict.keys())
+n = len(perm_tuple)
 
-permutations_dict = {''.join(val) : i for i, val in enumerate(it.permutations(letters))}
-permutations_list = list(permutations_dict)
-n = len(permutations_list)
-
-result_list = []
-div = n
-end = 0
-for k in range(1, size):
-    div //= size - k + 1
-    for j in range(size - k):
-        begin = end
-        end += div
-        for i in range(begin, end):
-            temp_str = permutations_list[i][k:] + permutations_list[i][k-1::-1]
-            result_list += [(permutations_dict[temp_str], permutations_list[i][k-1::-1])]
-
+result_str = perm_tuple[0]
+arr = arr_range(n)
 i = 0
-result_str = permutations_list[0]
-for _ in range(n - 1):    
-    result_str += result_list[i][1]
-    i = result_list[i][0]
+temp_str = ''
+for _ in range(n // 2 - 1):
+    k = num_in_range(arr, i)
+    val = perm_tuple[i]
+    i = perm_dict[val[k:] + val[k-1::-1]]
+    temp_str += val[k-1::-1]
+    if len(temp_str) > 4000:
+        result_str += temp_str
+        temp_str = ''
+result_str += temp_str
+del perm_dict
+del perm_tuple
 
+result_str += result_str[-2::-1]
 print(f'len = {len(result_str)}')
 print(result_str)
